92cc090915fa21064b83b85a18f9a7306cfe9c7a
Magnus-Mage <romescha123@gmail.com>
2025-07-16 07:16:22 +0000

    Add: read and write in asm

---
+++ small-asm/read-write.s
@@ -0,0 +1,57 @@
+.intel_syntax noprefix
+
+SYS_READ	=	0	# read text from stdin
+SYS_WRITE	=	1	# write text to stdout
+SYS_EXIT	=	60	# terminate the program
+STDIN		= 	0	# standard input
+STDOUT		= 	1	# standard output
+
+# ------------------------------
+# @brief declare a buffer for the prompt string
+.section .bss
+	uinput_len	=	24			# 24 bytes for user input
+	uinput	     	:  	.skip uinput_len	# buffer for user input
+
+# ------------------------------
+# @brief some sys statements 
+.section .data
+	prompt		:	.asciz "Please input some text: "
+	prompt_len	=	.	- prompt
+	text		:	.asciz "You Wrote: "
+	text_len	=	.	- text
+
+# ------------------------------
+# @brief instructions for read and write string from terminal
+.section .text
+	.global _start
+
+_start:
+	mov rax, SYS_WRITE	# Print out the prompt
+	mov rdi, STDOUT
+	lea rsi, prompt
+	mov rdx, prompt_len
+	syscall
+
+	mov rax, SYS_READ	# Read the value from terminal
+	mov rdi, STDIN
+	lea rsi, uinput
+	mov rdx, uinput_len
+	syscall 		# -> RAX	
+	push rax 		# (1)
+	 
+	mov rax, SYS_WRITE	# Print out "You Wrote: "
+	mov rdi, STDOUT
+	lea rsi, text
+	mov rdx, text_len
+	syscall
+
+	pop rdx			# (1)
+	mov rsi, uinput		# print out the read string
+	mov rdi, STDOUT
+	mov rax, SYS_WRITE
+	syscall
+
+exit_code:
+	xor edi, edi		# sucessful exit
+	mov rax, SYS_EXIT
+	syscall
