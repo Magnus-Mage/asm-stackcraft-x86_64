394b15d1d5df4a990e41f48a91bd40ae4db77884
Magnus-Mage <romescha123@gmail.com>
2025-07-21 05:06:01 +0000

    Add: arrays implementation

---
+++ small-asm/arrays.s
@@ -0,0 +1,12 @@
+.intel_syntax noprefix
+
+SYS_WRITE	=	1	# write text to stdout
+SYS_READ	=	0	# read text from stdin
+STDIN		=	0	# standard input
+STDOUT		=	1	# standard output
+SYS_EXIT	=	60	# terminate the program
+
+#--------------------------------
+# @brief implementing an array for stdin and stdout
+
+
