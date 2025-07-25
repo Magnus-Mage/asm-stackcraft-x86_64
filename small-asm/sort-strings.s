d38abb020b9f61c33116df4d7533c1782148e713
Magnus-Mage <romescha123@gmail.com>
2025-07-25 07:04:41 +0000

    Add: Sorting strings(bubble sort)

---
+++ small-asm/sort-strings.s
@@ -0,0 +1,14 @@
+.intel_syntax noprefix
+
+SYS_WRITE	=	1	# write out to stdout
+STDOUT		=	1	# standard output
+SYS_READ	=	0	# read from stdin
+STDIN		=	0	# standard read
+SYS_EXIT	=	60	# terminate the program
+MAX_STRING_SIZE	=	64	# String size of 63 with last character for null terminator
+MAX_STRINGS	=	20
+ 
+#--------------------------------
+# @brief Sorting strings from input
+
+# @brief Prompts and output strings
